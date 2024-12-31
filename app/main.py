7e2d2059b4b7b8e98bb4024ad16cc3f639717959
martforge <131208352+martforge@users.noreply.github.com>
2024-12-31 13:38:22 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -10,9 +10,16 @@ def main():
         # Exit condition for 'exit 0'
         if command == 'exit 0':
             break
+        
+        # Handle 'echo' command: Print the rest of the command after 'echo'
+        if command.startswith('echo '):
+            # Print the part after 'echo '
+            print(command[5:])
+
 
         # Handle invalid commands
-        print(f"{command}: command not found")
+        else: 
+            print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
